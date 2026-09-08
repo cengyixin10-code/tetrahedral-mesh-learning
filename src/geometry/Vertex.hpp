d4884462781a28e5a9ddd5ddfac7fd2f96cffde3
Ethan <cengyixin10@gmail.com>
2026-09-08 16:22:38 +0800

    feat: add mesh data model

---
+++ src/geometry/Vertex.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "geometry/Vec3.hpp"
+
+#include <cstddef>
+
+namespace tetra {
+
+// V0.1 中，顶点编号始终等于它在 Mesh 顶点数组中的位置。
+using VertexId = std::size_t;
+
+struct Vertex {
+    VertexId id{};
+    Vec3 position{};
+};
+
+} // namespace tetra
+
